4208ef659e658380eb537b323f40fbf884aee48f
LucioDangelo <ldangelo@itba.edu.ar>
2022-05-19 11:14:12 -0300

    Arreglos Simulador Interactivo

--- clase-4-funciones-desafio/simuladorinteractivo.js
+++ clase-4-funciones-desafio/simuladorinteractivo.js
@@ -1,52 +1,75 @@
 //Calcula el precio final de un producto. Teniendo en cuenta el costo de envio por distancia, el IVA y un descuento.
 
-var button = document.querySelector('button');
-button.onclick = function() {
+//Boton
+var button = document.querySelector("button");
 
-let iniciar = prompt('Desea calcular un precio? Si/No')
-if (iniciar.includes("si")) {
+//funcion que convierte a minusculas todo lo que ingreso
+const normalizeInput = (mensaje) => prompt(mensaje).trim().toLocaleLowerCase();
 
-do{
-  
-function distancia (){
-  do{
-     const zona = prompt('Ingresa la zona de entrega');
-     switch (zona) {
-         case 'a1':
-             precioZona = 10;
+//funcion iva
+const iva = (a) => a * 0.21;
+
+//funcion distancia, en donde se calcula entre varias zonas
+const distancia = () => {
+  do {
+    const zona = normalizeInput("Ingresa la zona de entrega");
+    switch (zona) {
+      case "a1":
+        precioZona = 10;
         break;
-         case 'b1':
-            precioZona = 15;
-         break;
-         case 'c1':
-            precioZona = 20;
-         break;
-         default:
-         alert(`Ingresa una zona existente. Haga referencia al mapa de zonas de nuestra web`);
-            precioZona = "A";
-         
-     }
-  } while (isNaN(precioZona))
+      case "b1":
+        precioZona = 15;
+        break;
+      case "c1":
+        precioZona = 20;
+        break;
+      default:
+        alert(
+          `Ingresa una zona existente. Haga referencia al mapa de zonas de nuestra web`
+        );
+        precioZona = "A";
     }
-
-    distancia();
-
-const suma = (a,b) => a + b
-const resta = (a,b) => a - b
-const iva = (a) => a * 0.21
+  } while (isNaN(precioZona));
+};
 
 
-let precioProducto = Number(prompt('ingresa el precio del producto'));
-let descuento = Number(prompt('ingresa el descuento del producto'));
 
-let precioFinal = resta(suma(precioZona,suma(precioProducto, iva(precioProducto))),descuento);
+//logica del boton
+button.onclick = function () {
+  let iniciar = normalizeInput("Desea calcular un precio? Si/No");
+  if (iniciar.includes("si")) {
+    do {
+      distancia();
 
-console.log("El precio final a pagar es de: " + precioFinal + "$ (Precio producto sin IVA: " + precioProducto + "$, Descuento: " + descuento + "$, Precio de Envio: " + precioZona + "$ )");
-  
-  let iniciar2 = prompt('Desea calcular otro precio? Si/No')
-  iniciar = iniciar + iniciar2
-} while (!iniciar.includes("no"))
-}
-else { console.log("Vuelva pronto")}
+      let precioProducto = Number(
+        normalizeInput("ingresa el precio del producto")
+      );
+      let descuento = Number(
+        normalizeInput("ingresa el descuento del producto")
+      );
 
-}
+      if (isNaN(precioProducto) || isNaN(descuento)) {
+        alert(`Porfavor ingrese datos numericos`);
+      } else {
+        let precioFinal = precioZona + iva(precioProducto) - descuento;
+
+        alert(
+          "El precio final a pagar es de: " +
+            precioFinal +
+            "$ (Precio producto sin IVA: " +
+            precioProducto +
+            "$, Descuento: " +
+            descuento +
+            "$, Precio de Envio: " +
+            precioZona +
+            "$ )"
+        );
+      }
+
+      let iniciar2 = normalizeInput("Desea calcular otro precio? Si/No");
+      iniciar = iniciar + iniciar2;
+    } while (!iniciar.includes("no"));
+  } else {
+    alert("Vuelva pronto");
+  }
+};
